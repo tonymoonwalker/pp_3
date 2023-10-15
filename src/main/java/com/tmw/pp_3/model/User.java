eb1abd18988c53a0d7c5f75e46862a298c722572
tonymoonwalker <tonymoonwalker@users.noreply.github.com>
2023-10-15 19:40:54 +0300

    init commit +validation

--- src/main/java/com/tmw/pp_3/model/User.java
+++ src/main/java/com/tmw/pp_3/model/User.java
@@ -1,7 +1,15 @@
 package com.tmw.pp_3.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
@@ -11,10 +19,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "firstname")
+    @Pattern(regexp = "^[a-zA-Z]+$")
     private String firstname;
     @Column(name = "lastname")
+    @Pattern(regexp = "^[a-zA-Z]+$")
     private String lastname;
     @Column(name = "email")
+    @Email
     private String email;
 
     public void setId(Long id) {
