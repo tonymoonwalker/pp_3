b55e098522e8dc397c4b4bb04a803880da21eff7
tonymoonwalker <tonymoonwalker@users.noreply.github.com>
2023-10-17 21:54:30 +0300

    Transactional added

--- src/main/java/com/tmw/pp_3/repository/UserRepository.java
+++ src/main/java/com/tmw/pp_3/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.tmw.pp_3.repository;
 
 import com.tmw.pp_3.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/tmw/pp_3/service/UserServiceImp.java
+++ src/main/java/com/tmw/pp_3/service/UserServiceImp.java
@@ -4,6 +4,7 @@ import com.tmw.pp_3.model.User;
 import com.tmw.pp_3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -16,21 +17,25 @@ public class UserServiceImp implements UserService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findById(Long id) {
         return userRepository.getReferenceById(id);
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> allUsers() {
         return userRepository.findAll();
     }
